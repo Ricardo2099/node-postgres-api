82a73776827686f3555fbbc0da647af5575c0fca
Ricardo2099 <ricardorojas104@gmail.com>
2025-04-26 14:12:06 -0600

    fix: asegurar rutas users en router principal

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers');
 
+
 router.get('/users', controller.getUsers);
 router.post('/users', controller.createUser);
 router.delete('/users/:id', controller.deleteUser);
