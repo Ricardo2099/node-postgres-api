145383fcc581389b387aeb60570ac811390b23ec
Ricardo2099 <ricardorojas104@gmail.com>
2025-04-26 11:16:05 -0600

    fix: usar process.env.DEV_DATABASE_URL correctamente en config.js

--- database/config/config.js
+++ database/config/config.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    username: "postgres",
-    password: "123456",
-    database: "postgresusersapi",
-    host: "127.0.0.1",
-    dialect: "postgres"
+    url: process.env.DEV_DATABASE_URL,
+    dialect: 'postgres',
+  },
+  production: {
+    url: process.env.DEV_DATABASE_URL,
+    dialect: 'postgres',
   }
-}
+};
